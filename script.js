3ca1666c3a679aae0716034327b9af149660b316
Caleb Asher <caleb@lytemine.com>
2016-03-04 12:17:17 -0700

    finished version with bug where buttonArray won't empty on reset.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
 	var buttonArray =[];
 	var gameReady = false;
+	var strict = false;
 	var counter = 1;
 	var buttonPresses = 0;
 	var j = 0;
@@ -12,31 +13,56 @@ $(document).ready(function(){
 	};
 
 	function generateArray(){
+		buttonArray = [];
 		for(i=0;i<20;i++){
 			var randomButton = Math.floor(Math.random() * 4 + 1);
 			buttonArray.push(randomButton);
 		}
-		console.log(buttonArray);
 	}
 
 	function cycleColor() {
-	$('#btn'+buttonArray[j-1]+'').toggleClass("regularColor"+buttonArray[j-1]+"");
-	$('#btn'+buttonArray[j-1]+'').toggleClass("lightenColor"+buttonArray[j-1]+"");
-	$('#btn'+buttonArray[j]+'').toggleClass("regularColor"+buttonArray[j]+"");
-	$('#btn'+buttonArray[j]+'').toggleClass("lightenColor"+buttonArray[j]+"");
+		$('#btn'+buttonArray[j]+'').toggleClass("regularColor"+buttonArray[j]+"");
+		$('#btn'+buttonArray[j]+'').toggleClass("lightenColor"+buttonArray[j]+"");
+		setTimeout(function(){
+			$('#btn'+buttonArray[j-1]+'').toggleClass("regularColor"+buttonArray[j-1]+"");
+			$('#btn'+buttonArray[j-1]+'').toggleClass("lightenColor"+buttonArray[j-1]+"");
+		}, 1500);
 	}
 
-	$('.bigbtn').click(function(){
+	function playback() {
 	
-		console.log("whatyoupressed:"+this.id);
-		console.log("buttonArray[buttonPresses]"+"btn"+buttonArray[buttonPresses]);
-		console.log(buttonPresses);
+		if (j == 0) {
+			console.log('playBack1 fired');
+			cycleColor();
+			$.playSound(sounds[buttonArray[j]]);
+			j++;
+			setTimeout(function(){	
+				playback();
+			}, 2000);
+		}
+
+		else if (j < counter) {
+			console.log('playBack2 fired');
+			cycleColor();
+			$.playSound(sounds[buttonArray[j]]);
+			j++;
+			setTimeout(function(){
+				playback();
+			}, 2000);
+		}
+
+		else if (j == counter) {
+			console.log('playBack3 fired');
+			j = 0;
+		}	
+	}
+
+		$('.bigbtn').click(function(){
 
 		if (this.id === "btn"+buttonArray[buttonPresses] && gameReady === true){
-			console.log("matched!");
+
 			$.playSound(sounds[buttonArray[buttonPresses]]);
 			buttonPresses++;
-			console.log("presses:"+buttonPresses);
 
 			if (buttonPresses == 20) {
 				$.playSound("http://www.soundjay.com/human/applause-01");
@@ -47,62 +73,62 @@ $(document).ready(function(){
 				counter++;
 				$('#counterFont').html(counter);
 				buttonPresses = 0;
-				playback();		
+				setTimeout(function(){
+					playback();
+				}, 2000);			
 			}	
 		}
 		else if (this.id !== "btn"+buttonArray[buttonPresses] && gameReady === true) {
-			console.log("did not match!");
-			buttonPresses = 0;
-			$.playSound("http://www.soundjay.com/misc/fail-buzzer-04");
-			playback();
+
+			if (strict === true) {
+				$.playSound("http://www.freesfx.co.uk/rx2/mp3s/5/5612_1335272419");
+				generateArray();
+				buttonPresses = 0;
+				counter = 1;
+				$('#counterFont').html('--');
+			}
+
+			else { 
+				buttonPresses = 0;
+				$.playSound("http://www.freesfx.co.uk/rx2/mp3s/5/5612_1335272419");
+				setTimeout(function(){
+						playback();
+				}, 2000);
+			}
 		}
 	});
-
-	function playback() {
-		console.log('nothing');
-		console.log(j);
-		if (j == 0) {
-			console.log('playBack1 success!');
-			$('#btn'+buttonArray[j]+'').toggleClass("regularColor"+buttonArray[j]+"");
-			$('#btn'+buttonArray[j]+'').toggleClass("lightenColor"+buttonArray[j]+"");
-			$.playSound(sounds[buttonArray[j]]);
-			j++;
-			setInterval(function(){		
-				playBack();	
-			}, 1000);
-		}
-
-		else if (j < counter) {
-			console.log('playBack2 success!');
-			cycleColor()
-			$.playSound(sounds[buttonArray[j]]);
-			j++;
-			setInterval(function(){		
-				playBack();	
-			}, 1000);
-		}
-
-		else if (j == counter) {
-			console.log('playBack3 success!');
-			clearInterval(setInterval(function(){playBack();}, 1000));
-			j = 0;
-		}	
-	}
 
 	$('#switchButton').click(function(){
 
 		$(this).toggleClass("on");
 		$('#counterFont').toggleClass("red");
-		buttonArray=[];
 		counter = 1;
+		buttonPresses = 0;
 		gameReady = false;
+		if (strict === true) {
+			strict = false; 
+			$('#strictLight').css("background-color", "#421717"); 
+		} 
 		$('#counterFont').html('--');
 		generateArray();
+		console.log(buttonArray);	
 
 		$('#startButton').click(function(){
 			playback();
 			gameReady = true;
-			$('#counterFont').html(counter);	
+			$('#counterFont').html(counter);
+		});
+
+		$('#strictButton').click(function(){
+			
+			if (strict === false) {
+				strict = true;
+				$('#strictLight').css("background-color", "red");
+			}
+			else if (strict === true) {
+				strict = false;
+				$('#strictLight').css("background-color", "#421717");
+			}
 		});
 
 	});
